32b5a122aee0ba70534318131f9e6af6e9097c78
Francois Stephany <francois@tamere.eu>
2019-12-03 21:51:53 +0100

    Day 02: Part Two
    This is starting to look like we're building an emulator/VM. Yay! \o/

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{Read, BufReader, BufRead};
 use std::fmt;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Error {
     /// Opcode is neither 1, 2 or 99. Indicate the faulty opcode position and
     /// its value.
@@ -25,7 +25,7 @@ impl fmt::Display for Error {
 
 fn main() {
     let input = File::open("day-02/gravity-assist-src.txt").expect("Could not open file");
-    let mut src = read_program(input);
+    let src = read_program(input);
 
     // Once you have a working computer, the first step is to restore the 
     // gravity assist program (your puzzle input) to the "1202 program alarm" 
@@ -34,15 +34,23 @@ fn main() {
     // replace position 2 with the value 2. What value is left at position 0 
     // after the program halts?
 
-    src[1] = 12;
-    src[2] = 2;
+    println!("Running the `1202 program alarm` with noun=12 and verb=2");
+    match run(&src, 12, 2) {
+        Ok(result) => println!("Result: {}", result),
+        Err(e) => println!("Error while running: {}", e),
+    }
 
-    match execute(&src) {
-        Ok(output) => {
-            println!("Output: {:?}", output);
-            println!("Position 0: {}", output[0]);
+
+    let target = 19690720;
+    println!("Brute force to find the verb and noun that will produce: {}", target);
+    
+    for noun in 0..100 {
+        for verb in 0..100 {
+            if run(&src, noun, verb) == Ok(target) { 
+                println!("--> noun={}, verb={}", noun, verb);
+                break;
+            }
         }
-        Err(e) => println!("An error occured: {}", e)
     }
 }
 
@@ -52,36 +60,39 @@ fn read_program<R: Read>(to_read: R) -> Vec<usize> {
         .map(|chars| {
             let chars: Vec<u8> = chars.unwrap();
             let token = std::str::from_utf8(&chars).expect("Invalid file content");
-            dbg!(token);
             token.parse::<usize>().expect("Token is not a number")
         })
         .collect()
 }
 
-/// We don't modify the input in place but returns a new copy. Not the most
-/// efficient but simple enough.
-fn execute(input: &Vec<usize>) -> Result<Vec<usize>, Error> {
+fn run(input: &Vec<usize>, noun: usize, verb: usize) -> Result<usize, Error> {
     let mut data = input.clone();
+    data[1] = noun;
+    data[2] = verb;
+    execute(&mut data)?;
 
+    Ok(data[0])
+}
+
+
+fn execute(input: &mut Vec<usize>) -> Result<(), Error> {
     // Command has the following schema:
     // OPCODE A_POS B_POS RESULT_POS
-    // - Opcode 1: a + b 
-    // - Opcode 2: a * b 
-    for current in (0..data.len()).step_by(4) {
-        let opcode = *data.get(current).ok_or_else(|| Error::IncompleteCommand)?;
+    for current in (0..input.len()).step_by(4) {
+        let opcode = *input.get(current).ok_or_else(|| Error::IncompleteCommand)?;
 
         if opcode == 99 {
             // returns immediately, there is no need to go any further.
-            return Ok(data);
+            return Ok(());
         }
 
-        let a_pos = *data.get(current + 1).ok_or_else(|| Error::IncompleteCommand)?;
-        let b_pos = *data.get(current + 2).ok_or_else(|| Error::IncompleteCommand)?;
-        let res_pos = *data.get(current + 3).ok_or_else(|| Error::IncompleteCommand)?;
+        let a_pos = *input.get(current + 1).ok_or_else(|| Error::IncompleteCommand)?;
+        let b_pos = *input.get(current + 2).ok_or_else(|| Error::IncompleteCommand)?;
+        let res_pos = *input.get(current + 3).ok_or_else(|| Error::IncompleteCommand)?;
 
         // Fetch the data themselves
-        let a = *data.get(a_pos).ok_or_else(|| Error::PointerAccessError)?;
-        let b = *data.get(b_pos).ok_or_else(|| Error::PointerAccessError)?;
+        let a = *input.get(a_pos).ok_or_else(|| Error::PointerAccessError)?;
+        let b = *input.get(b_pos).ok_or_else(|| Error::PointerAccessError)?;
 
         let result = match opcode {
             1 => a + b,
@@ -89,10 +100,10 @@ fn execute(input: &Vec<usize>) -> Result<Vec<usize>, Error> {
             _ => return Err(Error::InvalidOpcode(opcode, current))
         };
 
-        data[res_pos] = result;
+        input[res_pos] = result;
     }
 
-    Ok(data)
+    Ok(())
 }
 
 #[cfg(test)]
@@ -100,11 +111,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn output_test() {    
-        assert_eq!(execute(&vec![1,0,0,0,99]).unwrap(), vec![2,0,0,0,99]);
-        assert_eq!(execute(&vec![2,3,0,3,99]).unwrap(), vec![2,3,0,6,99]);
-        assert_eq!(execute(&vec![2,4,4,5,99,0]).unwrap(), vec![2,4,4,5,99,9801]);
-        assert_eq!(execute(&vec![1,1,1,4,99,5,6,0,99]).unwrap(), vec![30,1,1,4,2,5,6,0,99]);
+    fn output_test() {
+        let mut input = vec![1,0,0,0,99];
+        let _ = execute(&mut input);
+        assert_eq!(input, vec![2,0,0,0,99]);
+        
+        let mut input = vec![2,3,0,3,99];
+        let _ = execute(&mut input);
+        assert_eq!(input, vec![2,3,0,6,99]);
+        
+        let mut input = vec![2,4,4,5,99,0];
+        let _ = execute(&mut input);
+        assert_eq!(input, vec![2,4,4,5,99,9801]);
+        
+        let mut input = vec![1,1,1,4,99,5,6,0,99];
+        let _ = execute(&mut input);
+        assert_eq!(input, vec![30,1,1,4,2,5,6,0,99]);
+    }
+
+    #[test]
+    fn run_test() {
+        let input = File::open("day-02/gravity-assist-src.txt").expect("Could not open file");
+        let src = read_program(input);
+        assert_eq!(run(&src, 12, 2).unwrap(), 3516593)
     }
 
     #[test]
