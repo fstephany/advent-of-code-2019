75544c7322ac184badcc9054cf360f15c03227b0
Francois Stephany <francois@tamere.eu>
2019-12-10 21:02:45 +0100

    Day 05: Identify Opcode and ParamMode
    Small coding session between dinner and dessert :p

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -6,11 +6,11 @@ use std::io::{BufRead, BufReader, Read};
 pub enum Error {
     InvalidSourceCode,
     /// Opcode is an unknown value. 
-    /// Indicate the faulty opcode value and position
-    InvalidOpcode(isize, usize),
+    /// Indicate the faulty opcode value
+    InvalidOpcode(isize),
     /// A ParamMode is invalid. It either a wrong value or an impossible value
     /// (e.g., Immediate mode for a write operation)
-    InvalidParamMode(isize, usize),
+    InvalidParamMode(isize),
     /// A command is not complete. Eg., missing parameters for a given operation
     IncompleteCommand,
     /// Error while trying to access a memory address
@@ -21,42 +21,77 @@ impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Error::InvalidSourceCode => write!(f, "Error while reading source code"),
-            Error::InvalidOpcode(v, p) => write!(f, "InvalidOpcode {} at position {})", v, p),
-            Error::InvalidParamMode(v, p) => write!(f, "InvalidParamMode. Opcode: {} at position {})", v, p),
+            Error::InvalidOpcode(v) => write!(f, "InvalidOpcode: {})", v),
+            Error::InvalidParamMode(v) => write!(f, "InvalidParamMode. Opcode decl.: {})", v),
             Error::IncompleteCommand => write!(f, "Incomplete Command"),
             Error::PointerAccessError => write!(f, "Invalid pointer address"),
         }
     }
 }
 
+#[derive(PartialEq, Debug)]
 pub enum Opcode {
     Add = 1,         // OPCODE A B RESULT_POS
     Multiply = 2,    // OPCODE A B RESULT_POS
     StoreInput = 3,  // OPCODE A where A is the location where to store input 
     Output = 4,      // OPCODE A
+    Exit = 99,       // OPCODE
 }
 
 impl Opcode {
     pub fn parse(value: isize) -> Result<(Opcode, Vec<ParamMode>), Error> {
+        let opcode = match value % 100 {
+            // we could use the [num_enum](https://crates.io/crates/num_enum) 
+            // crate for this. 
+            1 => Opcode::Add,
+            2 => Opcode::Multiply,
+            3 => Opcode::StoreInput,
+            4 => Opcode::Output,
+            99 => Opcode::Exit,
+            _ => return Err(Error::InvalidOpcode(value))
+        };
 
+        let mut modes = vec![ParamMode::Position; opcode.size() - 1];
+        
+        // Get the number of digits of value
+        // let digits = (value as f32).log10().floor() as isize + 1;
+
+        for i in 0..modes.len() {
+            // Get the i-th digit in the value.
+            // In the case of implicit modes (i.e., the mode of some parameters
+            // is not specified), we fallback to 0 which is the default mode 
+            // anyway. 
+            let ten = 10 as isize; 
+            let digit = (value % ten.pow(i as u32 + 3)) / ten.pow(i as u32 + 2); 
+
+            modes[i] = match digit {
+                0 => ParamMode::Position,
+                1 => ParamMode::Immediate,
+                _ => return Err(Error::InvalidParamMode(value))
+            };
+        }
+
+        Ok((opcode, modes))
     }
 
     /// The total size of the instruction (opcode + parameters)
     pub fn size(&self) -> usize {
         match self {
-            Add => 4,
-            Multiply => 4,
-            StoreInput => 2,
-            Output => 2
+            Opcode::Add => 4,
+            Opcode::Multiply => 4,
+            Opcode::StoreInput => 2,
+            Opcode::Output => 2,
+            Opcode::Exit => 1
         }
     }
 }
 
 /// Beware that Parameters that are used to *store* a value are of course always
 /// in Position mode
+#[derive(PartialEq, Debug, Clone, Copy)]
 pub enum ParamMode {
-    Immediate, // Value is provided by the param itself 
     Position,  // Value is stored at the given address 
+    Immediate, // Value is provided by the param itself 
 }
 
 pub struct Param {
@@ -141,34 +176,21 @@ impl Run {
                 return Ok(self.memory[0]);
             }
 
-            let a_pos = *self
-                .memory
-                .get(current + 1)
-                .ok_or_else(|| Error::IncompleteCommand)?;
-            let b_pos = *self
-                .memory
-                .get(current + 2)
-                .ok_or_else(|| Error::IncompleteCommand)?;
-            let res_pos = *self
-                .memory
-                .get(current + 3)
-                .ok_or_else(|| Error::IncompleteCommand)?;
+            let a_pos = self.get_mem(current + 1)?;
+            let b_pos = self.get_mem(current + 2)?;
+            let res_pos = self.get_mem(current + 3)?;
 
             // Fetch the data themselves
-            let a = *self.memory
-                .get(a_pos)
-                .ok_or_else(|| Error::PointerAccessError)?;
-            let b = *self.memory
-                .get(b_pos)
-                .ok_or_else(|| Error::PointerAccessError)?;
+            let a = self.get_mem(a_pos as usize)?;
+            let b = self.get_mem(b_pos as usize)?;
 
             let result = match opcode {
                 1 => a + b,
                 2 => a * b,
-                _ => return Err(Error::InvalidOpcode(opcode, current)),
+                _ => return Err(Error::InvalidOpcode(opcode)),
             };
 
-            self.memory[res_pos] = result;
+            self.memory[res_pos as usize] = result;
         }
 
         Ok(self.memory[0])
@@ -204,6 +226,32 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn opcode_parse_test() {
+        let (opcode, modes) = Opcode::parse(1002).unwrap();
+        let expected_modes = vec![
+            ParamMode::Position,   // 0
+            ParamMode::Immediate,  // 1
+            ParamMode::Position    // This one is implicit
+        ];
+        assert_eq!(opcode, Opcode::Multiply); // 02
+        assert_eq!(modes, expected_modes);
+
+        let (opcode, modes) = Opcode::parse(99).unwrap();
+        let expected_modes = Vec::new();
+        assert_eq!(opcode, Opcode::Exit);
+        assert_eq!(modes, expected_modes);
+
+        let (opcode, modes) = Opcode::parse(01).unwrap();
+        let expected_modes = vec![
+            ParamMode::Position, // Implicit
+            ParamMode::Position, // Implicit
+            ParamMode::Position  // Implicit
+        ];
+        assert_eq!(opcode, Opcode::Add);
+        assert_eq!(modes, expected_modes);
+    }
 
     #[test]
     fn output_test() {
